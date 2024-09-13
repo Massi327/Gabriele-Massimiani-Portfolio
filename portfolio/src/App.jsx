38fbaa24b2be0477dd5fd57bef484e51dcd12b1c
Massi327 <115101665+Massi327@users.noreply.github.com>
2024-09-13 15:38:57 +0200

    [ADD] Card

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, Row, Col } from 'react-bootstrap';
 import './App.css'
 import Header from './Header'
+import Card from 'react-bootstrap/Card'
 
 function App() {
 
@@ -36,33 +37,21 @@ function App() {
           </p>
           </Col>
         </Row>
+
         <Row className='section m-5'>
           <Col>
-          <h1 id='about'>About</h1>
-          <p className='p'>
-            During my years at the Politecnico di
-            Torino I was able to combine both
-            engineering and creative skills. 
-          </p>
-          <p className='p'>
-            I had the chance to take part in 5+ projects in
-            which I explored multiple roles such as
-            producer, project coordinator, and
-            programmer. <br/> This had been important to
-            test how to work in a team, manage time
-            better, make a budget plan and how to
-            make a good pitch.
-          </p>
-          <p className='p'>
-            I am an organised, precise and goal-oriented person, and I
-            always give my best. <br/> I would like to work
-            in the video game market, which I follow
-            as an enthusiast and user, but I am open
-            to jobs that allow me to test my skills and
-            learn more and more.
-          </p>
+          <Card className='card'>
+            <Card.Img variant="top" src="src/Asset/LUL_FROG_Complete_Name.png" className="card-img-small"/>
+            <Card.Body>
+              <Card.Title>Level Up Lab</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
           </Col>
         </Row>
+
         <Row className='section m-5'>
           <Col>
           <h1 id='projects'>Projects</h1>
@@ -90,6 +79,7 @@ function App() {
           </p>
           </Col>
         </Row>
+
         <Row className='section m-5'>
           <Col>
           <h1 id='contact'>Contact</h1>
