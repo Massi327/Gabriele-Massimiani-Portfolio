3b75ab54fc6f36cfbbaf29611f30ada91f319a59
Massi327 <115101665+Massi327@users.noreply.github.com>
2024-09-16 18:42:46 +0200

    [ADD] Footer

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -34,13 +34,13 @@ function App() {
     {
       title: 'COMPUTER GRAPHICS (University Project - 2023)', 
       img: 'src/Asset/Computer Grafica.png', 
-      url: 'https://www.dropbox.com/sh/fjle2b7s5uz53gt/AAA4iSq1DHIa9hGZljldnr2-a?dl=0',
+      url: 'https://drive.google.com/drive/folders/1ExdxCFqfp_qRhEA5NKu8tBZz6U6Zeihh?usp=sharing',
       description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     },
     {
       title: 'COMPUTER ANIMATION (University Project - 2023)', 
       img: 'src/Asset/Computer Animation.png', 
-      url: 'https://www.dropbox.com/sh/fjle2b7s5uz53gt/AAA4iSq1DHIa9hGZljldnr2-a?dl=0',
+      url: 'https://drive.google.com/drive/folders/1_HJ8RKqDarBg5fbkirwpYArqR78tFC_6?usp=sharing',
       description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     },
     {
@@ -52,7 +52,7 @@ function App() {
     {
       title: 'HUMAN COMPUTER INTERACTION (University Project - 2023)', 
       img: 'src/Asset/APPy family.png', 
-      url: 'https://www.dropbox.com/scl/fi/9dpr5tni9gc4pcbblkkie/APPy-Family-Final-Group-Report.pdf?rlkey=wq0pvi7ylnhm63dlbb81n62db&dl=0',
+      url: 'https://drive.google.com/drive/folders/1HvJw8jJ-kHlRQ4E_pqUz4vJm_PtwBvn1?usp=sharing',
       description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     },
 
@@ -66,34 +66,29 @@ function App() {
     <>
       <Header/>
       <Container className="home">
+        
+        {/*About*/}
         <Row className='section m-5 pt-4'>
           <Col>
           <h1>Hi, I'm <span className='red-text'>Gabriele Massimiani!</span></h1>
           <p className='p'>
-            During my years at the Politecnico di
-            Torino I was able to combine both
-            engineering and creative skills. 
+           TODO
           </p>
-          <p className='p'>
-            I had the chance to take part in 5+ projects in
-            which I explored multiple roles such as
-            producer, project coordinator, and
-            programmer. <br/> This had been important to
-            test how to work in a team, manage time
-            better, make a budget plan and how to
-            make a good pitch.
-          </p>
-          <p className='p'>
-            I am an organised, precise and goal-oriented person, and I
-            always give my best. <br/> I would like to work
-            in the video game market, which I follow
-            as an enthusiast and user, but I am open
-            to jobs that allow me to test my skills and
-            learn more and more.
-          </p>
+          
+          <h2>Skills & Tools</h2>
+          <p className='p'>Blender, Unity, Unreal Engine, Notion, Jira, Suite Microsoft, Suite Adobe, Java, Javascript (React), HTML, C#, C, SQL</p>
           </Col>
         </Row>
 
+        
+
+        {/*Projects*/}
+        <Row className='section m-5'>
+          <Col>
+          <h1 id='projects'>Projects</h1>
+          <p className='p'>TODO</p>
+          </Col>
+        </Row>
         <Row className='section m-5 d-flex justify-content-center'>
           {projects.map((project, index) => (
             <Col key={index} xs={12} sm={6} md={4} lg={3} className="d-flex justify-content-center mb-4">
@@ -109,66 +104,26 @@ function App() {
             </Col>
           ))}
         </Row>
-
-        <Row className='section m-5'>
-          <Col>
-          <h1 id='projects'>Projects</h1>
-          <p className='p'>
-            During my years at the Politecnico di
-            Torino I was able to combine both
-            engineering and creative skills. 
-          </p>
-          <p className='p'>
-            I had the chance to take part in 5+ projects in
-            which I explored multiple roles such as
-            producer, project coordinator, and
-            programmer. <br/> This had been important to
-            test how to work in a team, manage time
-            better, make a budget plan and how to
-            make a good pitch.
-          </p>
-          <p className='p'>
-            I am an organised, precise and goal-oriented person, and I
-            always give my best. <br/> I would like to work
-            in the video game market, which I follow
-            as an enthusiast and user, but I am open
-            to jobs that allow me to test my skills and
-            learn more and more.
-          </p>
-          </Col>
-        </Row>
-
+        
+        {/*Contacts*/}
         <Row className='section m-5'>
           <Col>
           <h1 id='contact'>Contact</h1>
           <p className='p'>
-            During my years at the Politecnico di
-            Torino I was able to combine both
-            engineering and creative skills. 
-          </p>
-          <p className='p'>
-            I had the chance to take part in 5+ projects in
-            which I explored multiple roles such as
-            producer, project coordinator, and
-            programmer. <br/> This had been important to
-            test how to work in a team, manage time
-            better, make a budget plan and how to
-            make a good pitch.
-          </p>
-          <p className='p'>
-            I am an organised, precise and goal-oriented person, and I
-            always give my best. <br/> I would like to work
-            in the video game market, which I follow
-            as an enthusiast and user, but I am open
-            to jobs that allow me to test my skills and
-            learn more and more.
+            TODO
           </p>
           </Col>
         </Row>
       </Container>
       
       <footer className='footer'>
-        <p>© 2024 Gabriele Massimiani. All rights reserved.</p>
+        <div className="footer-content">
+          <a href='https://www.linkedin.com/in/gabriele-massimiani/'>
+          <img src='src/Asset/Linkedin logo.png' alt="LinkedIn" className="footer-logo" />
+          LinkedIn
+          </a>
+          <span>© 2024 Gabriele Massimiani. All rights reserved.</span>
+        </div>
       </footer>
     </>
   )
