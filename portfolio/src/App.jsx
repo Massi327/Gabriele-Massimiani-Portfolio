eff788e7f6fae7c6edbe121f5d1b45782a80f827
Massi327 <115101665+Massi327@users.noreply.github.com>
2024-09-18 18:28:38 +0200

    [FIX] Fix env

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -110,14 +110,11 @@ function App() {
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
-    console.log(process.env.REACT_APP_EMAILJS_SERVICE_ID);
-    console.log(process.env.REACT_APP_EMAILJS_TEMPLATE_ID);
-    console.log(process.env.REACT_APP_EMAILJS_YOUR_USER_ID);
     emailjs.sendForm(
-      process.env.REACT_APP_EMAILJS_SERVICE_ID, 
-      process.env.REACT_APP_EMAILJS_TEMPLATE_ID, 
+      import.meta.env.VITE_EMAILJS_SERVICE_ID, 
+      import.meta.env.VITE_EMAILJS_TEMPLATE_ID, 
       form.current, 
-      process.env.REACT_APP_EMAILJS_YOUR_USER_ID)
+      import.meta.env.VITE_EMAILJS_YOUR_USER_ID)
       .then((result) => {
           console.log(result.text);
       }, (error) => {
