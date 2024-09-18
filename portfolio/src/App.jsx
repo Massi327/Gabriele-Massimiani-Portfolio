2273ee49b90f1a5158390ada7d070cb2af52b25d
Massi327 <115101665+Massi327@users.noreply.github.com>
2024-09-18 19:02:22 +0200

    [UPDATE] cambiata posizione img

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const projects = [
     {
       title: 'Level Up Lab', 
-      img: 'src/Asset/LUL_FROG_Complete_Name.png', 
+      img: 'public/Asset/LUL_FROG_Complete_Name.png',
       url: 'https://www.leveluplab.it/',
       description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     },
@@ -22,7 +22,7 @@ function App() {
         GLOBAL GAME JAM (2024)
       </>
     ), 
-      img: 'src/Asset/Clownageddon.png', 
+      img: 'public/Asset/Clownageddon.png', 
       url: 'https://abstractborderstudio.itch.io/clownageddon',
       description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     },
@@ -33,7 +33,7 @@ function App() {
         FAIL/SAFE GAME JAM
         </>
     ), 
-      img: 'src/Asset/Line Spacing.png', 
+      img: 'public/Asset/Line Spacing.png', 
       url: 'https://abstractborderstudio.itch.io/linespacing',
       description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     },
@@ -44,19 +44,19 @@ function App() {
         (University Project - 2023)
         </>
       ), 
-      img: 'src/Asset/Game Design.png', 
+      img: 'public/Asset/Game Design.png', 
       url: 'https://drive.google.com/drive/folders/1xQgwhUWqW5u1B2fjd0F76i_cZb1jxiJW?usp=sharing',
       description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     },
     {
       title: 'Computer Graphics (University Project - 2023)', 
-      img: 'src/Asset/Computer Grafica.png', 
+      img: 'public/Asset/Computer Grafica.png', 
       url: 'https://drive.google.com/drive/folders/1ExdxCFqfp_qRhEA5NKu8tBZz6U6Zeihh?usp=sharing',
       description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     },
     {
       title: 'Computer Animation (University Project - 2023)', 
-      img: 'src/Asset/Computer Animation.png', 
+      img: 'public/Asset/Computer Animation.png', 
       url: 'https://drive.google.com/drive/folders/1_HJ8RKqDarBg5fbkirwpYArqR78tFC_6?usp=sharing',
       description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     },
@@ -67,7 +67,7 @@ function App() {
         (University Project - 2023)
         </>
       ),  
-      img: 'src/Asset/The Sky Postman.png', 
+      img: 'public/Asset/The Sky Postman.png', 
       url: 'https://cutt.ly/LwvVVqop',
       description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     },
@@ -78,7 +78,7 @@ function App() {
         (University Project - 2023)
         </>
       ),    
-      img: 'src/Asset/APPy family.png', 
+      img: 'public/Asset/APPy family.png', 
       url: 'https://drive.google.com/drive/folders/1HvJw8jJ-kHlRQ4E_pqUz4vJm_PtwBvn1?usp=sharing',
       description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     },
@@ -86,21 +86,20 @@ function App() {
   ]
 
   const skillLogo = [
-    {src: 'src/Asset/unity-logo.png', alt: 'Unity'},
-    {src: 'src/Asset/unreal-engine-logo.png', alt: 'Unreal Engine'},
-    {src: 'src/Asset/blender-logo.png', alt: 'Blender'},
-    {src: 'src/Asset/notion-logo.png', alt: 'Notion'},
-    {src: 'src/Asset/jira-logo.png', alt: 'Jira'},
-    {src: 'src/Asset/javascript-logo.svg', alt: 'Javascript'},
-    {src: 'src/Asset/adobe-logo.png', alt: 'Adobe'},
-    {src: 'src/Asset/bootstrap-logo.svg', alt: 'Bootstrap'},
-    {src: 'src/Asset/c-logo.svg', alt: 'C'},
-    {src: 'src/Asset/csharp.png', alt: 'Csharp'},
-    {src: 'src/Asset/css-logo.svg', alt: 'CSS'},
-    {src: 'src/Asset/html-logo.svg', alt: 'HTML'},
-    {src: 'src/Asset/Microsoft-logo.svg', alt: 'Microsoft'},
-    {src: 'src/Asset/react-logo.svg', alt: 'React'},
-    {src: 'src/Asset/sqlite-logo.svg', alt: 'Sqlite'},
+    {src: 'public/Asset/unity-logo.png', alt: 'Unity'},
+    {src: 'public/Asset/unreal-engine-logo.png', alt: 'Unreal Engine'},
+    {src: 'public/Asset/blender-logo.png', alt: 'Blender'},
+    {src: 'public/Asset/notion-logo.png', alt: 'Notion'},
+    {src: 'public/Asset/jira-logo.png', alt: 'Jira'},
+    {src: 'public/Asset/javascript-logo.svg', alt: 'Javascript'},
+    {src: 'public/Asset/adobe-logo.png', alt: 'Adobe'},
+    {src: 'public/Asset/bootstrap-logo.svg', alt: 'Bootstrap'},
+    {src: 'public/Asset/c-logo.svg', alt: 'C'},
+    {src: 'public/Asset/csharp.png', alt: 'Csharp'},
+    {src: 'public/Asset/css-logo.svg', alt: 'CSS'},
+    {src: 'public/Asset/html-logo.svg', alt: 'HTML'},
+    {src: 'public/Asset/react-logo.svg', alt: 'React'},
+    {src: 'public/Asset/sqlite-logo.svg', alt: 'Sqlite'},
   ]
 
   const handleCardClick = (url) => {
@@ -197,7 +196,7 @@ function App() {
       <footer className='footer'>
         <div className="footer-content">
           <a href='https://www.linkedin.com/in/gabriele-massimiani/'>
-          <img src='src/Asset/Linkedin logo.png' alt="LinkedIn" className="footer-logo" />
+          <img src='public/Asset/Linkedin logo.png' alt="LinkedIn" className="footer-logo" />
           LinkedIn
           </a>
           <span>© 2024 Gabriele Massimiani. All rights reserved.</span>
