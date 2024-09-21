152c5135a7b5049fa0991500507372c5d8e59b26
Massi327 <115101665+Massi327@users.noreply.github.com>
2024-09-21 18:42:32 +0200

    [ADD] ROUTES

---
+++ portfolio/src/LevelUpLab.jsx
@@ -0,0 +1,9 @@
+
+export default function LevelUpLab() {
+    return (
+        <div>
+        <h1>LevelUpLab</h1>
+        <p>LevelUpLab is a new way to learn and grow your skills as a developer.</p>
+        </div>
+    );
+}
