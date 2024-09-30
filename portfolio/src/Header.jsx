852c9ebd5324d4f2118022683abff37386d1fd0f
Massi327 <115101665+Massi327@users.noreply.github.com>
2024-09-30 16:56:55 +0200

    [UPDATE] Prima sistemazione per telefono

--- portfolio/src/Header.jsx
+++ portfolio/src/Header.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { Container, Navbar, Nav, Dropdown } from "react-bootstrap";
 import './App.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'; // Importazione delle icone
+
 
 export default function Header(props) {
 
@@ -26,16 +28,30 @@ export default function Header(props) {
     return (
         <Navbar className="navbar" data-bs-theme='dark'>
         <Container>
-            <a href={href} className='navbar-brand' >
-            <img alt="Logo_GM" src="/Gabriele-Massimiani-Portfolio/assets/Logo_GM.svg" className="navbar-brand-logo"/>
+            <a href={href} className='navbar-brand'>
+                <img alt="Logo_GM" src="/Gabriele-Massimiani-Portfolio/assets/Logo_GM.svg" className="navbar-brand-logo"/>
+            </a>
+            <a href={href} className='navbar-brand d-none d-lg-block'>
                 Gabriele Massimiani | Portfolio
             </a>
             {homePage && 
-                <Nav className="ms-auto">
+            <>
+                <Nav className="ms-auto d-none d-lg-flex">
                     <Nav.Link href="#projects" className="navlink">Projects</Nav.Link>
                     <Nav.Link href="#contact" className="navlink">Contact</Nav.Link>
                     <Nav.Link href="/Gabriele-Massimiani-Portfolio/assets/CV GABRIELE MASSIMIANI.pdf" className="navlink">Resume</Nav.Link>
                 </Nav>
+
+                <Dropdown align={{ lg: 'end' }} className='d-lg-none'>
+                <Dropdown.Toggle className="dropdown-outline" variant=''>
+                    <i className="bi bi-list"></i>
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item >Storico Partite</Dropdown.Item>
+                    <Dropdown.Item >Logout</Dropdown.Item>
+                </Dropdown.Menu>
+                </Dropdown>
+                </>
             }
         </Container>
         </Navbar>
