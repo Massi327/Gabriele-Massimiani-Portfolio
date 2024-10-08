728f62bf9ad9ee2e787ab549dd52291281f0ca54
Massi327 <115101665+Massi327@users.noreply.github.com>
2024-10-08 19:41:32 +0200

    [UPDATE] Aggiunti iframe a Game Jam

--- portfolio/src/GameJam.jsx
+++ portfolio/src/GameJam.jsx
@@ -6,30 +6,47 @@ export default function GameJam() {
         <>
             <Row className='section mt-5 mx-3 pt-4'>
               <h1><span className='red-text'>Game Jam</span></h1>
-              <h2>UI Programmer & Game Designer</h2>
+            </Row>
+            <Row className='section mx-3 pt-1 mt-2'>
+              <h2>Global Game Jam 2024</h2>
             </Row>
             <Row className='section mx-4 pt-3'>
-                <Col xs={12} sm={12} md={12} lg={4}>
+                <Col xs={12} sm={12} md={12} lg={5}>
                     <p className='p'>
                     TODO
                     </p>
+                    <iframe src="https://itch.io/embed/2496957?bg_color=E1E2D0&amp;fg_color=101419&amp;link_color=D81919&amp;border_color=E1E2D0" className='embededItch'><a href="https://abstractborderstudio.itch.io/clownageddon">Clownageddon by AbstractBorderStudio, IlariaPasini, plummycake, nyolind, Gabriele Massimiani, Pietro</a></iframe>
                 </Col>
-                <Col xs={12} sm={12} md={12} lg={8}>
-                    <img src='/Gabriele-Massimiani-Portfolio/assets/LUL_Press_Kit.png' alt='Level Up Lab Logo' className='imgPageProjectDx'/>
+                <Col xs={12} sm={12} md={12} lg={7}>
+                <div className="video-container-dx">
+                        <iframe 
+                            src="https://www.youtube.com/embed/ZV5B9T892ec" 
+                            title="Clowageddon trailer" 
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                            allowFullScreen
+                        ></iframe>
+                </div>               
                 </Col>
             </Row>
 
             <Row className='section mx-3 pt-1 mt-2'>
-              <h2>Role & Structure</h2>
+              <h2>Fail/Safe Game Jam 2023</h2>
             </Row>
             <Row className='section mx-4 pt-1'>
-                <Col xs={12} sm={12} md={12} lg={8}>
-                    <img src='/Gabriele-Massimiani-Portfolio/assets/NotionProgram Plan.png' alt='Level Up Lab Logo' className='imgPageProjectSx'/>
-                </Col>
-                <Col xs={12} sm={12} md={12} lg={4}>
+                <Col xs={12} sm={12} md={12} lg={7}>
+                <div className="video-container-sx">
+                    <iframe 
+                        src="https://www.youtube.com/embed/gx0rGHIhqkE" 
+                        title="LineSpacing - Fail/Safe game jam entry" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                        allowFullScreen
+                    ></iframe>
+                </div>                    </Col>
+                <Col xs={12} sm={12} md={12} lg={5}>
                     <p className='p mt-2'>
                     TODO
                     </p>
+                    <iframe src="https://itch.io/embed/2296794?bg_color=E1E2D0&amp;fg_color=101419&amp;link_color=D81919&amp;border_color=E1E2D0" className='embededItch'><a href="https://abstractborderstudio.itch.io/linespacing">LineSpacing by AbstractBorderStudio, Gabriele Massimiani, Pietro</a></iframe>
                 </Col>
             </Row>
         </>
